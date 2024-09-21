0662c19126861188524870d6a860eb1943932527
Takashi Matsuno <gonsuke@users.noreply.github.com>
2024-09-21 16:55:40 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+
+async function run(): Promise<void> {
+  try {
+    // Get the 'run' input
+    const runCommand = core.getMultilineInput('run').join('\n');
+
+    // Execute the commands
+    const options: exec.ExecOptions = {
+      listeners: {
+        stdout: (data: Buffer) => {
+          console.log(data.toString());
+        },
+        stderr: (data: Buffer) => {
+          console.error(data.toString());
+        }
+      }
+    };
+
+    await exec.exec('bash', ['-c', runCommand], options);
+
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+}
+
+run();
