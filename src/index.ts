197941171051f50d275b74afdb1440852f255abf
Takashi Matsuno <gonsuke@users.noreply.github.com>
2024-09-21 17:12:34 +0900

    feat: @actions/exec

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
     // Get the 'run' input
-    const runCommand = core.getMultilineInput('run').join('\n');
+    const runCommand = core.getMultilineInput('run').join('\n')
 
     // Execute the commands
     const options: exec.ExecOptions = {
       listeners: {
         stdout: (data: Buffer) => {
-          console.log(data.toString());
+          console.log(data.toString())
         },
         stderr: (data: Buffer) => {
-          console.error(data.toString());
+          console.error(data.toString())
         }
       }
-    };
+    }
 
-    await exec.exec('bash', ['-c', runCommand], options);
-
+    await exec.exec('bash', ['-c', runCommand], options)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-run();
+run()
